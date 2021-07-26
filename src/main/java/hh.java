d8763d6cf4c65372f7943b092feded9931230043
Mohmoody <syed.mahmoody2014@gmail.com>
2021-07-26 01:27:47 -0400

    1st Commit

--- src/main/java/hh.java
+++ src/main/java/hh.java
@@ -1,24 +1,15 @@
+import java.util.Arrays;
+
 public class hh {
-    public static void main(String[] args) {
-       System.out.println(getGreatestCommonDivisor(27, 18));
 
 
-    }
+        public static void main(String[] args) {
+            int[] array = { 1, 2, 6, 9, -2, 4, -3, -8, 7,6 };
+            Arrays.sort(array);
+            System.out.println(array[array.length-1]);
+//            for (int i=0; i<=array.length; i++){
+//                System.out.println(array[0]);
+//            }
 
-    public static int getGreatestCommonDivisor(int first, int second) {
-        if (first < 10 || second < 10) {
-            return -1;
         }
-        int gcd = 1;
-        int min = first;
-        if (first > second) {
-            min = second;
-        }
-        for (int i = 1; i <= min; i++) {
-            if (first % i == 0 && second % i == 0) {
-                gcd = i;
-            }
-        }
-        return gcd;
-    }
 }
